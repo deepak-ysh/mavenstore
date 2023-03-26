0a4d9adda39d63f1ff7e26bad4b4f584c6b69179
deepak <jangadeepak9@gmail.com>
2023-03-26 15:32:50 +0530

    making chnages in pom.xml

---
+++ src/main/java/in/Deepak/app/App.java
@@ -0,0 +1,13 @@
+package in.Deepak.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
